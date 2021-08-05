31e04f382ee78899aab29d7d3da87d81baadda38
trongdatbkhn <75197265+trongdatbkhn@users.noreply.github.com>
2021-08-05 13:00:11 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+getJokes();
+
+jokeBtn.addEventListener('click', getJokes)
+ 
+async function getJokes() {
+
+    const config = {
+        headers: {
+            "Accept": "application/json"
+        }
+    }
+    const res = await  fetch('https://icanhazdadjoke.com', config);
+    const data = await res.json();
+    joke.innerText = data.joke;
+}
+
+// jokeBtn.addEventListener('click', getJokes)
+
+// function generateJoke() {
+
+//     const config = {
+//         headers: {
+//             "Accept": "application/json"
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//     .then(res => res.json())
+//     .then(data => {
+//         joke.innerText = data.joke;
+//     })
+// }
